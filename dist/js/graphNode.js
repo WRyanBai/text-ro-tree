9d5866f6d2cfd4d517c5590df4bfc7feeac58279
Ryan Bai <jxbyp@yahoo.com>
2022-03-13 01:20:16 +0800

    modularized dist folder

---
+++ dist/js/graphNode.js
@@ -0,0 +1,81 @@
+import{treeNode} from './treeNode.js';
+import {formatGraph, bulletPtTree, canvas, graphXMargin, graphYMargin} from './main.js';
+
+export class graphNode{
+	constructor(treeNode){
+		this.graphElement = document.createElement('div');
+		this.graphElement.classList.add('graphNode');
+		canvas.appendChild(this.graphElement);
+		this.graphText = document.createElement('div');
+		this.graphText.classList.add('graphText');
+		this.graphElement.appendChild(this.graphText);
+		this.xCoord = 0;
+		this.yCoord = 0;
+		
+		let graphElementStyles = window.getComputedStyle(this.graphElement);
+		
+		this.graphWidth = graphElementStyles.width;
+		this.graphWidth = parseInt(this.graphWidth.substring(0, this.graphWidth.length - 2));
+		this.graphHeight = graphElementStyles.height;
+		this.graphHeight = parseInt(this.graphHeight.substring(0, this.graphHeight.length - 2));
+		this.padding = graphElementStyles.padding;
+		this.padding = parseInt(this.padding.substring(0, this.padding.length - 2));
+		this.lineHeight = window.getComputedStyle(this.graphText).lineHeight;
+		this.lineHeight = parseInt(this.lineHeight.substring(0, this.lineHeight.length - 2));
+	}
+	
+	getGraphElement(){
+		return(this.graphElement);
+	}
+	
+	setGraphCoord(xCoord, yCoord){
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+		this.graphElement.style.left = xCoord.toString() + 'px';
+		this.graphElement.style.top = yCoord.toString() + 'px';
+	}
+	
+	getXCoord(){
+		return(this.xCoord);
+	}
+	
+	getYCoord(){
+		return(this.yCoord);
+	}
+	
+	getGraphWidth(){
+		return(this.graphWidth);
+	}
+	
+	setGraphWidth(graphWidth){
+		this.graphWidth = graphWidth;
+		this.graphElement.style.width = graphWidth.toString() + 'px';
+	}
+	
+	getGraphHeight(){
+		return(this.graphHeight);
+	}
+	
+	setGraphHeight(graphHeight){
+		this.graphHeight = graphHeight;
+		this.graphElement.style.height = graphHeight.toString() + 'px';
+	}
+	
+	getText(){
+		return(this.graphText.textContent);
+	}
+	
+	setText(newText){
+		this.graphText.textContent = newText;
+		let newHeight = window.getComputedStyle(this.graphText).height;
+		newHeight = parseInt(newHeight.substring(0, newHeight.length - 2));
+		newHeight += 2 * this.padding;
+		if (newHeight != this.graphHeight){
+			if(newHeight === 2 * this.padding){
+				newHeight += this.lineHeight;
+			}
+			this.setGraphHeight(newHeight);
+			formatGraph(bulletPtTree.getRootNode().getChildren(), 0, 0, graphXMargin, graphYMargin);
+		}
+	}
+}
---
+++ dist/js/treeNode.js
@@ -0,0 +1,137 @@
+import {graphNode} from './graphNode.js';
+import{graphLine} from './graphLine.js';
+import {selectBulletPt, handleKeyPress, textChange} from './main.js';
+
+export class treeNode{
+	constructor(bulletPtSection){
+		if (bulletPtSection != undefined){
+			this.bulletPtSection = bulletPtSection;
+			this.bulletPtText = this.bulletPtSection.lastElementChild;
+			this.children = [];
+			this.graphNode = new graphNode(this);
+			this.bulletPtSection.addEventListener('click', selectBulletPt);
+			this.bulletPtText.addEventListener('keydown', handleKeyPress);
+			this.bulletPtText.addEventListener('keyup', textChange);
+			this.margin = window.getComputedStyle(bulletPtSection).marginLeft;
+			this.graphLines = [];
+		}
+		else{
+			this.margin = '0px';
+			this.children = [];
+		}
+		
+	}
+	
+	setParent(parentNode){
+		this.parentNode = parentNode;
+		if (parentNode.margin != undefined){
+			this.margin = parentNode.getMargin();
+			this.addMargin();
+		}
+	}
+	
+	getParent (){
+		return(this.parentNode);
+	}
+	
+	getBulletPtSection(){
+		return(this.bulletPtSection);
+	}
+	
+	getBulletPtText(){
+		return(this.bulletPtText);
+	}
+	
+	appendChildNode(childNode, i){
+		//this method adds a node to the children array of this node
+		childNode.setParent(this);
+		//If the second parameter 'i' is not inputted, the node is added to the end of the array.
+		if (i === undefined){
+			this.children.push(childNode);
+			if(this.graphLines != undefined){
+				this.graphLines.push(new graphLine(this, childNode));
+			}
+		}else{
+			//If the parameter 'i' is inputted, the node will be appended at the index i
+			this.children.splice(i, 0, childNode)
+			if(this.graphLines != undefined){
+				this.graphLines.splice(i, 0, new graphLine(this, childNode));
+			}
+		}
+	}
+	
+	removeChildNode(input){
+		let i = 0;
+		let childrenNumber = this.children.length;
+		if (typeof input === 'number'){
+			i = input;
+		}
+		else{
+			for(i; i<childrenNumber; i++){
+				if (this.children[i]===input){
+					break;
+				}
+			}
+		}
+		if(this.graphLines != undefined){
+			this.graphLines[i].removeLine();
+		}
+		for(i; i<childrenNumber-1; i++){
+			this.children[i] = this.children[i+1];
+			if(this.graphLines != undefined){
+				this.graphLines[i] = this.graphLines[i+1];
+			}
+		}
+		this.children.pop();
+		if(this.graphLines != undefined){
+			this.graphLines.pop();
+		}
+	}
+	
+	getChildren(){
+		return(this.children);
+	}
+	
+	getSiblingIndex(){
+		//Find the index of this node in its parent's children array
+		let i = 0;
+		let siblingNumbers = this.parentNode.getChildren().length;
+		for(i; i<siblingNumbers; i++){
+			if (this.parentNode.getChildren()[i] === this){
+				break;
+			}
+		}
+		return (i);
+	}
+	
+	getMargin(){
+		return(this.margin);
+	}
+	
+	setMargin(margin){
+		this.margin = margin;
+		this.bulletPtSection.style.marginLeft = margin;
+	}
+	
+	addMargin(){
+		//adds 20 pixels to the left margin of the bulletPtSection html element
+		this.margin = (parseInt(this.margin.substring(0,this.margin.length - 2))+ 20).toString() + 'px';
+		this.bulletPtSection.style.marginLeft = this.margin;
+	}
+	
+	getGraphNode(){
+		return(this.graphNode);
+	}
+	
+	getGraphLines(){
+		return(this.graphLines);
+	}
+	
+	getText(){
+		return(this.bulletPtText.textContent);
+	}
+	
+	setText(newText){
+		this.BulletPtText.textContent = newText;
+	}
+}
